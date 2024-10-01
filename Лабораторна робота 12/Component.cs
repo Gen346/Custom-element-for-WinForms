d1102fad80cd5a4729cc0b62c59f3dad0dc305fb
Gen346 <kelzreez@gmail.com>
2024-10-01 13:19:02 +0300

    Add project files.

---
+++ Лабораторна робота 12/Component.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Лабораторна_робота_12
+{
+    public class CustomTimer : Timer
+    {
+        public Control _komponent { get; set; }
+
+        public event EventHandler OnVisibles;
+
+        public CustomTimer()
+        {
+            this.Tick += new EventHandler(OnTick);
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            Visibles();
+        }
+
+        public void Visibles()
+        {
+            if (_komponent != null)
+            {
+                _komponent.Visible = !_komponent.Visible;
+                OnVisibles?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ Лабораторна робота 12/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Лабораторна_робота_12
+{
+    public partial class Form1 : Form
+    {
+        private CustomTimer customTimer;
+        private Button button;
+        private Label label;
+        private bool isLabelVisible = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ComponentLoader();
+        }
+        private void ComponentLoader()
+        {
+            button = new Button
+            {
+                Text = "Click Me",
+                Location = new System.Drawing.Point(50, 50)
+            };
+            button.Click += Button_Click;
+            this.Controls.Add(button);
+
+            label = new System.Windows.Forms.Label
+            {
+                Text = "Hello, World!",
+                Location = new System.Drawing.Point(50, 100)
+            };
+            this.Controls.Add(label);
+
+            customTimer = new CustomTimer
+            {
+                Interval = 3000, // In ms;
+                _komponent = label
+            };
+            customTimer.OnVisibles += CustomTimer_OnVisibles;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (isLabelVisible)
+            {
+                customTimer.Start();
+            }
+            else
+            {
+                customTimer.Start();
+            }
+        }
+
+        private void CustomTimer_OnVisibles(object sender, EventArgs e)
+        {
+            isLabelVisible = !isLabelVisible;
+            customTimer.Stop();
+        }
+    }
+}
